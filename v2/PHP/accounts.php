49e75c70f883ce6d210bd7c8f35633957acbc841
Indulgentu <indulgentul@gmail.com>
2019-02-11 02:01:11 +0200

    Add files via upload

---
+++ v2/PHP/accounts.php
@@ -0,0 +1,13 @@
+<?php
+
+	$mysqli = new mysqli('localhost', 'alexbamm_root', 'nissan123', 'alexbamm_unitbv');
+	$q = "SELECT * FROM `bambank_accounts` WHERE `userid`='" . $mysqli->real_escape_string($_GET['userid']) . "'";
+	if(!$query = $mysqli->query($q)) { die(json_encode(array("result" => "NOT_OK", "error_msg" => $mysqli->error))); }
+	$arr = array();
+	$i = 0;
+	while ($row = $query->fetch_assoc()) {
+		$arr[$i] = array("id" => $row['id'], "IBAN" => $row['IBAN'], "type" => $row['type'], "accID" => $row['accID'], "currency" =>  $row['currency'], "userid" => $row['userid'], "balance" => $row['balance']);
+		$i++;
+	}
+	die(json_encode(array("result" => "OK", "accounts" => $arr)));
+?>
